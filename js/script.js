31c06fa9c2e084f23d0155dfddaed81ba7422e5c
Fariz abdulfatah <farizabdufatah245@gmail.com>
2025-01-23 12:24:41 +0700

    navbar fix

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// navtime
+
+function timer(){
+    let timewrap = document.querySelector(".time");
+    const now = new Date();
+
+    let hours = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = minute < 10 ? "0" + second : second;
+
+    timewrap.innerHTML = `${hours}H : ${minute}M : ${second}S`;
+}
+
+setInterval(timer, 1000);
+
+timer();
