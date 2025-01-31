3eab59b7ca0830c840274386a8f80436b856e25c
Fariz abdulfatah <farizabdufatah245@gmail.com>
2025-01-31 22:44:23 +0700

    menambahkan fitur

--- js/script.js
+++ js/script.js
@@ -19,3 +19,26 @@ setInterval(timer, 1000);
 
 timer();
 
+// kanan main page region
+const buttons = document.querySelectorAll(".kanan .size .chart .region button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function(){ 
+    buttons.forEach(btn => btn.classList.remove("active"));
+    this.classList.add("active");
+    }
+    );}
+);
+
+// kanan main page size
+const buttonss = document.querySelectorAll(".kanan .size .chart .sizeshoes button");
+
+buttonss.forEach(button => {
+    button.addEventListener("click", function(){ 
+    buttonss.forEach(btn => btn.classList.remove("active"));
+    this.classList.add("active");
+    }
+    );}
+);
+
+// kanan main page quantity
